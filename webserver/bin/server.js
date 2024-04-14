797d73a87947f0604241c6a3144486bea283c95f
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-14 11:15:35 +0300

    [CHORE] add message http route

--- webserver/bin/server.js
+++ webserver/bin/server.js
@@ -2,5 +2,5 @@ import app from '../index.js'
 
 const port = 8000;
 app.listen(port, () => {
-    console.log('Server has been started!');
+    console.log('Server has been started -> http://localhost:8000');
 })
