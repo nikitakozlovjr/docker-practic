41a4c0d9b877b4070269a4a7cf17c7a7d1e8e339
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-15 22:03:41 +0300

    [CHORE] add user routerApiStat

--- webserver/index.js
+++ webserver/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import routerRedirect from './src/routes/routes.redirect.js';
+import routerApiStatistic from './src/api/api.routes.statistic.js';
 
 const app = express();
 app.set('view engine', 'pug');
 
 app.use(routerRedirect);
+app.use('/api', routerApiStatistic);
 
 export default app;
