24e50487fca7ef0eb5b3e4f379402efcfbc7b0a8
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-15 22:11:35 +0300

    [CHORE] update import path

--- utility/bin/footballStat.js
+++ utility/bin/footballStat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import FootballStatService from "../src/footballStat.server.js";
+import FootballStatService from "../src/FootballStatService.js";
 const paramStatistic = process.argv[2]
 
 const statistic = await new FootballStatService(paramStatistic).getStat();
